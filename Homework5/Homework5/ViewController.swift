aa12aec90af4044f1df7184b09c48125b15086f4
Colin Hoffman <colin@Colins-MacBook-Pro.local>
2018-03-23 14:01:18 -0500

    Finished the homeowork

--- Homework5/Homework5/ViewController.swift
+++ Homework5/Homework5/ViewController.swift
@@ -10,8 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stackView: UIStackView!
+    
+    override func viewWillAppear(_ animated: Bool)
+    {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
